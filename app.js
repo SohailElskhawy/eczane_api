0bea27cdd20c78c29d6e4dfdc2b3d882cfc8f205
SohailElskhawy <sohailelskhawy2@gmail.com>
2024-07-05 17:05:49 +0300

    fix

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 const scrapeDrugs = async () => {
     const baseUrl = 'https://www.ilacabak.com/aralist.php?Id=';
